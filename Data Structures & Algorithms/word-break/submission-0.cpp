80feb626414f1d55eb33b92a7f6ff56860caba43
Krishnapal Zala <krishnapalzalagithub1807@gmail.com>
2026-04-20 16:28:51 +0530

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    bool dfs(string& s, vector<string>& wordDict, int i, vector<int>& dp){
+        if(dp[i] != -1) return dp[i];
+
+        for(auto word: wordDict){
+            if(s.size() - i >= word.size() && s.substr(i, word.size()) == word){
+                dp[i] = dfs(s, wordDict, word.size() + i, dp);
+                if(!dp[i]) continue;
+                return dp[i];
+            }
+        }
+        return dp[i] = 0;
+    }
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<int> dp(s.size() + 1, -1);
+        dp[s.size()] = 1;
+        return dfs(s, wordDict, 0, dp);
+    }
+};
